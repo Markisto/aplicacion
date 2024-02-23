00c095d07965a529ffeed37dc5c9c9763bf311ad
Rojo Lazcano <36898654+Markisto@users.noreply.github.com>
2024-02-23 11:17:21 -0600

    v22-02-2024

--- src/app/home/pedidos/pedidos.component.ts
+++ src/app/home/pedidos/pedidos.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component  } from '@angular/core';
+
 
 @Component({
   selector: 'app-pedidos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pedidos.component.html',
   styleUrl: './pedidos.component.css'
 })
 export class PedidosComponent {
+
+  vista = 'consulta';
 
 }
