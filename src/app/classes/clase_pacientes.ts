96a4834c02354bfbd399840b9c104f8a75b84a55
Rojo Lazcano <36898654+Markisto@users.noreply.github.com>
2024-02-29 22:46:12 -0600

    v-29-02-2024

---
+++ src/app/classes/clase_pacientes.ts
@@ -0,0 +1,16 @@
+export class C_Pacientes{
+    nombre_paciente: string ="";
+    edad_años: number = 0;
+    edad_meses: number = 0;
+    fecha_nacimiento: string = "";
+    receta = "";
+    dosis = "";
+    cartucho = "";
+    cubre = "";
+    compra = "";
+    cubre_total = "";
+    presentacion = "";
+    precio = "";
+
+    
+}
--- src/app/home/clientes/clientes.component.ts
+++ src/app/home/clientes/clientes.component.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import 'bootstrap-table';
+import 'bootstrap-table/dist/extensions/mobile/bootstrap-table-mobile.min.js';
+import Swal from 'sweetalert2';
+import { C_Pacientes } from '../../classes/clase_pacientes';
 
 
 @Component({
@@ -9,7 +13,42 @@ import { Component } from '@angular/core';
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css'
 })
-export class ClientesComponent {
+export class ClientesComponent implements OnInit {
+ 
+
+
   vista = "nuevo";
+  pacientes : C_Pacientes[] = [];
+  inittable = 1;
+  
+  constructor() {
+    
+   }
+
+  ngOnInit(): void {
+     
+     this.Agregar_Paciente();
+  }
+
+  ngAfterViewChecked(){
+    if(this.inittable < 2){
+      this.inittable++;
+      $('#table_new_paciente').bootstrapTable({
+        classes: 'table table-sm table-bordered',
+      });
+      let pas = new C_Pacientes();
+    pas.nombre_paciente = "Carmen";
+    this.pacientes.push(pas);
+    $('#table_new_paciente').bootstrapTable('append', this.pacientes);
+    }
+  }
+
+  Agregar_Paciente(){
+    let pas = new C_Pacientes();
+    pas.nombre_paciente = "Carmen";
+    this.pacientes.push(pas);
+    $('#table_new_paciente').bootstrapTable('append', this.pacientes);
+  }
+
 }
  
